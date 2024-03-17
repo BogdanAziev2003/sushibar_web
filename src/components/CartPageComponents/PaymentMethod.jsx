5cfb385fead84face6c889602b1e347a8a5cabcf
tays001 <z.taysaev2003@gmail.com>
2024-03-17 12:14:10 +0300

    cart components add

---
+++ src/components/CartPageComponents/PaymentMethod.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import styles from '../../pages/CartPage/Cart.module.scss';
+
+const PaymentMethod = () => {
+  return (
+    <div className={styles.method}>
+      <div className={styles.method__text}>
+        <p>Способ оплаты</p>
+      </div>
+
+      <div className={styles.method__list}>
+        <div className={styles.method__el}>
+          <div className={`${styles.method__button}`}></div>
+          <div>
+            <p>Наличными</p>
+          </div>
+        </div>
+        <div className={styles.method__el}>
+          <div
+            className={`${styles.method__button__pressed} ${styles.method__button}`}
+          >
+            <svg
+              width="10"
+              height="8"
+              viewBox="0 0 10 8"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M3.81295 7.24815C3.58895 7.24815 3.36495 7.16315 3.19395 6.99215L0.820953 4.61915C0.478953 4.27715 0.478953 3.72315 0.820953 3.38215C1.16295 3.04015 1.71595 3.03915 2.05795 3.38115L3.81295 5.13615L7.94095 1.00815C8.28295 0.666148 8.83595 0.666148 9.17795 1.00815C9.51995 1.35015 9.51995 1.90415 9.17795 2.24615L4.43195 6.99215C4.26095 7.16315 4.03695 7.24815 3.81295 7.24815Z"
+                fill="white"
+              />
+            </svg>
+          </div>
+          <div>
+            <p>Переводом</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PaymentMethod;
--- src/pages/CartPage/index.jsx
+++ src/pages/CartPage/index.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
+import styles from './Cart.module.scss';
+import Phone from '../../components/CartPageComponents/Phone';
+import PaymentMethod from '../../components/CartPageComponents/PaymentMethod';
+import DeliveryMethod from '../../components/CartPageComponents/DeliveryMethod';
+import Comment from '../../components/CartPageComponents/Comment';
+import CountPribor from '../../components/CartPageComponents/CountPribor';
+
 const CartPage = () => {
-  return <div>index</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.order__text}>
+        <p>Оформление заказа</p>
+      </div>
+
+      <CountPribor />
+      <Phone />
+      <PaymentMethod />
+      <DeliveryMethod />
+      <Comment />
+    </div>
+  );
 };
 
 export default CartPage;
