1f5baf4ba146d9ecd0b05a3b2e3fc8641d9f722e
tays001 <z.taysaev2003@gmail.com>
2024-03-22 13:32:58 +0300

    skeleton fixed styled

--- src/components/Layout/Skeleton.jsx
+++ src/components/Layout/Skeleton.jsx
@@ -7,14 +7,14 @@ export const Skeleton = (props) => (
     width={335}
     height={285}
     viewBox="0 0 335 285"
-    backgroundColor="#ffffff"
-    foregroundColor="#dedede"
+    backgroundColor="#F3F3F3"
+    foregroundColor="#FFF"
     {...props}
   >
     <rect x="0" y="0" rx="10" ry="10" width="335" height="150" />
-    <rect x="-1" y="160" rx="5" ry="5" width="115" height="15" />
-    <rect x="0" y="180" rx="5" ry="5" width="50" height="10" />
-    <rect x="0" y="200" rx="5" ry="5" width="104" height="20" />
+    <rect x="10" y="160" rx="5" ry="5" width="115" height="15" />
+    <rect x="10" y="180" rx="3" ry="3" width="50" height="10" />
+    <rect x="10" y="200" rx="5" ry="5" width="104" height="20" />
     <rect x="10" y="240" rx="9" ry="9" width="315" height="40" />
   </ContentLoader>
 );
