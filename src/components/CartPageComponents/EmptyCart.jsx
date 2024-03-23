ee7c18893fe4bcf2d45039a82bf5d06f01ff957f
tays001 <z.taysaev2003@gmail.com>
2024-03-23 21:34:50 +0300

    empty cart change image

--- src/components/CartPageComponents/EmptyCart.jsx
+++ src/components/CartPageComponents/EmptyCart.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import styles from '../../pages/CartPage/Cart.module.scss';
-import cartImage from '../../image/empty-cart.png';
+import cartImage from '../../image/empty-cart-test-1.png';
 
 const EmptyCart = () => {
   return (
