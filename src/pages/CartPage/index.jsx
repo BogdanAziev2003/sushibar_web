9776795b34ca4a8263d35cfb4a282e9592e2ec7d
tays001 <z.taysaev2003@gmail.com>
2024-03-23 22:59:07 +0300

    send data bug fixed

--- src/pages/CartPage/index.jsx
+++ src/pages/CartPage/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import { useTelegram } from '../../hooks/useTelegram';
